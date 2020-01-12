0737d7f421b34e8e1c67bf30194ac70d7f2621ab
Robert Spiegel <r.spiegel@netconomy.net>
2020-01-12 20:38:24 +0100

    Basic skeleton

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kindle-flashcards
 Set of utilities for  creating flashcards on Kindle
+
+.. work in progress..
+
+Currently mainly for private use.
---
+++ src/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs'),
+    es = require('event-stream'),
+    parse = require('csv-parse/lib/sync');
+
+const readCardDataFromFile = (fileName, cards) => {
+    let stream = fs.createReadStream(fileName)
+        .pipe(es.split())           // split on new line
+        .pipe(
+            es.mapSync(function(line) {
+                process.stdout.write(line);
+                cards.push(line);
+            })
+        );
+};
+
+const main = (argv) => {
+    if (argv.length < 2) {
+        process.stdout.write('invalid arguments');
+        process.exit(1);
+    }
+    const args = argv.slice(2);
+
+    const cards = [];
+    readCardDataFromFile(args, cards);
+};
+
+main(argv);
+
+
