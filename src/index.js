8bf16a63dcd98148756b3afe85a20b3fadeae20f
Robert Spiegel <r.spiegel@netconomy.net>
2020-01-14 21:39:45 +0100

    Basic CSV output

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 const
     fs = require('fs'),
     es = require('event-stream'),
-    parse = require('csv-parse/lib/sync'),
+    // https://www.npmjs.com/package/csv-parse
+    csvParse = require('csv-parse/lib/sync'),
+    csvStringify = require('csv-stringify'),
     translateText = require('@vitalets/google-translate-api'),
     Promise = require('bluebird');
 
+const CSV_DELIMITER = ';';
+
+const CSV_PARSE_OPTIONS = {
+    columns: false,
+    skip_empty_lines: true,
+    delimiter: CSV_DELIMITER
+};
+
+const CSV_STRINGIFY_OPTIONS = {
+    delimiter: CSV_DELIMITER
+};
+
+const CARD_COL_COUNT = 3;
+
+/**
+ * Read card data from CSV file.
+ *
+ * @param fileName
+ * @param cards
+ * @return {Promise}
+ */
 const readCardDataFromFile = (fileName, cards) => {
     return new Promise((resolve, reject) => {
 
@@ -22,14 +45,26 @@ const readCardDataFromFile = (fileName, cards) => {
                     lineNr += 1;
 
                     // process line here and call s.resume() when ready
-                    const parsedLine = parse(line, {
-                        columns: false,
-                        skip_empty_lines: true,
-                        delimiter: ';'
-                    });
-                    if (Array.isArray(parsedLine) && (parsedLine.length === 1)) {
-                        const oneParsedLine = parsedLine[0];
-                        cards.push(oneParsedLine);
+
+                    const parsedLines = csvParse(line, CSV_PARSE_OPTIONS);
+                    // we expect parsed line to have exactly one line
+                    // and dirst item is expected to be string
+                    const isLineValid = Array.isArray(parsedLines)
+                        && (parsedLines.length === 1)
+                        && Array.isArray(parsedLines[0])
+                        && typeof parsedLines[0][0] == 'string';
+
+                    if (isLineValid) {
+                        // this is the line we got
+                        const oneParsedLine = parsedLines[0];
+                        // and the value at the 1st column should be non blank string
+                        const keyword = oneParsedLine[0];
+                        if (keyword.length > 0) {
+                            while (oneParsedLine.length < CARD_COL_COUNT) {
+                                oneParsedLine.push('');
+                            }
+                            cards.push(oneParsedLine);
+                        }
                     }
 
                     // resume the readstream, possibly from a callback
@@ -49,24 +84,44 @@ const addTranslation = (cards) => {
     // see http://bluebirdjs.com/docs/api/promise.mapseries.html
     // or http://bluebirdjs.com/docs/api/promise.each.html
 
+    let cntTranslations = 0;
     return Promise.each(cards, (card) => {
-        if (!Array.isArray(card) || (card.length < 1)) {
-            return;
-        }
-        if (card.length < 2) {
-            card.push(null);
-        }
-        const text = card[0];
-        if (!card) {
-            return;
+        const validCard = Array.isArray(card)
+            && (card.length >= CARD_COL_COUNT)
+            && (typeof card[0] === 'string');
+
+        if (!validCard) {
+            return card;
         }
 
-        return translateText(text, {from: 'en', to: 'de'}).then((result) => {
-            console.log(result);
+        const keyword = card[0];
+        const translation = card[1];
+        const toBeTranslated = (!translation) || translation.length === 0;
+        if (!toBeTranslated) {
+            return card;
+        }
+
+        // TODO pass languages as parameters
+        return translateText(keyword, {from: 'en', to: 'de'}).then((result) => {
+            //console.log(result);
             card[1] = result.text;
+            cntTranslations++;
         });
     }).then(() => {
+        console.log(`${cntTranslations} translations added..`);
         return cards;
+    });
+};
+
+const writeToFile = (cards, fileName) => {
+    // stringify(records, [options], callback).
+    csvStringify(cards, CSV_STRINGIFY_OPTIONS, (err, output) => {
+        fs.writeFile(fileName, output, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log(`The file ${fileName} was saved.`);
+        });
     });
 };
 
@@ -75,12 +130,19 @@ const main = (argv) => {
         process.stdout.write('invalid arguments');
         process.exit(1);
     }
+    // TODO meaningful command line parsing
     const args = argv.slice(2);
 
     const cards = [];
-    readCardDataFromFile(args[0], cards).then(addTranslation).then(() => {
-        console.log(cards);
-    });
+
+    const inputFile = args[0];
+    const writeCardsToFile = () => writeToFile(cards, inputFile + '.new');
+    readCardDataFromFile(inputFile, cards)
+        .then(addTranslation)
+        .then(writeCardsToFile)
+        .then(() => {
+            console.log('All done.');
+        });
 };
 
 main(process.argv);
